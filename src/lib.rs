bedaa16c711d7838a4ad559b6508b2d57d427d6a
윤병익 <yeun0908@gmail.com>
2025-10-25 18:12:17 +0900

    drop no_std support

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 #![feature(const_trait_impl)]
 #![doc(issue_tracker_base_url = "https://github.com/andrewmilson/optimized-fields/issues/")]
 #![warn(clippy::all, clippy::pedantic, clippy::cargo, clippy::nursery)]
-#![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod fp31;
 pub mod fp64;
