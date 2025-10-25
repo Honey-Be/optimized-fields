34ee5478fd57ed6b01062df1fce92a75b21c6db6
윤병익 <yeun0908@gmail.com>
2025-10-25 18:16:41 +0900

    restore no_std support

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(const_trait_impl)]
 #![doc(issue_tracker_base_url = "https://github.com/andrewmilson/optimized-fields/issues/")]
 #![warn(clippy::all, clippy::pedantic, clippy::cargo, clippy::nursery)]
+#![cfg_attr(not(feature = "std"), no_std)]
 
 pub mod fp31;
 pub mod fp64;
